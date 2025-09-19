5177139c8e2db84cc26fe55d521431f58fd4bea9
yago-vasco <yago.rc.04@gmail.com>
2025-09-19 12:11:50 +0200

    Creación del servicio de hasheo

---
+++ src/main/java/com/YagoRueda/Finanzas/utils/BcryptHashser.java
@@ -0,0 +1,28 @@
+package com.YagoRueda.Finanzas.utils;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class BcryptHashser {
+
+    private PasswordEncoder encoder;
+
+    @Value("${security.bcrypt.strength:12}")
+    private int rounds;
+
+    @PostConstruct
+    public void init(){
+        this.encoder = new BCryptPasswordEncoder(rounds);
+    }
+
+    public String hash(String toHash) {
+        return encoder.encode(toHash);
+    }
+
+    public boolean match(String plaintext, String hash) {
+        return encoder.matches(plaintext, hash);
+    }
+
+}
