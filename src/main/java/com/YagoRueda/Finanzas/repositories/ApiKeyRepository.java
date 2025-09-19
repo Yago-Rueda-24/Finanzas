9254d63fd1e06d4f2e93e9ddbb7e281cd0a9d0af
yago rueda <yago.rc.04@gmail.com>
2025-09-19 17:00:58 +0200

    Implementación de query para obtener usuarios a partir de apikey

--- src/main/java/com/YagoRueda/Finanzas/repositories/ApiKeyRepository.java
+++ src/main/java/com/YagoRueda/Finanzas/repositories/ApiKeyRepository.java
@@ -1,9 +1,14 @@
 package com.YagoRueda.Finanzas.repositories;
 
 import com.YagoRueda.Finanzas.entities.ApiKeyEntity;
+import com.YagoRueda.Finanzas.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ApiKeyRepository extends JpaRepository<ApiKeyEntity,Long> {
 
     boolean existsByApiKey(String apikey);
+    @Query("SELECT a.user FROM ApiKeyEntity a WHERE a.apiKey = :apiKey")
+    UserEntity findByApiKey(@Param("apiKey") String apiKey);
 }
