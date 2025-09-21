b7b3cbf2af33916106f7485cd449b0953ac7092f
yago-vasco <yago.rc.04@gmail.com>
2025-09-21 21:37:17 +0200

    Creación de las funciones de recuperación de ApiKeys e Invalidación de apikeys de un usuario

--- src/main/java/com/YagoRueda/Finanzas/repositories/ApiKeyRepository.java
+++ src/main/java/com/YagoRueda/Finanzas/repositories/ApiKeyRepository.java
@@ -3,12 +3,21 @@ package com.YagoRueda.Finanzas.repositories;
 import com.YagoRueda.Finanzas.entities.ApiKeyEntity;
 import com.YagoRueda.Finanzas.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface ApiKeyRepository extends JpaRepository<ApiKeyEntity,Long> {
+public interface ApiKeyRepository extends JpaRepository<ApiKeyEntity, Long> {
 
     boolean existsByApiKey(String apikey);
+
+    ApiKeyEntity findByApiKey(String apikey);
+
     @Query("SELECT a.user FROM ApiKeyEntity a WHERE a.apiKey = :apiKey")
-    UserEntity findByApiKey(@Param("apiKey") String apiKey);
+    UserEntity findUserByApiKey(@Param("apiKey") String apiKey);
+
+    @Modifying
+    @Query("UPDATE ApiKeyEntity a SET a.validated = false WHERE a.validated = true AND a.user = :user")
+    void invalidateUserKeys(@Param("user") UserEntity user);
+
 }
