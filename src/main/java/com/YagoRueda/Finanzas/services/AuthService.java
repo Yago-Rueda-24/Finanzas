536d433c895552c8c27933f52814c9519e9789b0
yago-vasco <yago.rc.04@gmail.com>
2025-09-21 21:37:46 +0200

    Implementación de la invalidación de apikeys en los servicios de usuario

--- src/main/java/com/YagoRueda/Finanzas/services/AuthService.java
+++ src/main/java/com/YagoRueda/Finanzas/services/AuthService.java
@@ -7,6 +7,7 @@ import com.YagoRueda.Finanzas.repositories.ApiKeyRepository;
 import com.YagoRueda.Finanzas.repositories.UserRepository;
 import com.YagoRueda.Finanzas.utils.ApiKeyGenerator;
 import com.YagoRueda.Finanzas.utils.HashUtil;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -41,33 +42,43 @@ public class AuthService {
         return userRepository.save(user);
     }
 
+    @Transactional
     public String generateToken(UserDTO dto) throws IllegalArgumentException {
         if (!userRepository.existsByUsername(dto.getUsername())) {
             throw new IllegalArgumentException("Usuario o passwords incorrectos");
         }
         UserEntity user = userRepository.findByUsername(dto.getUsername());
-        boolean password_correct= hashser.BCryptMatch(dto.getPassword(), user.getPassword());
+        boolean password_correct = hashser.BCryptMatch(dto.getPassword(), user.getPassword());
         if (!password_correct) {
             throw new IllegalArgumentException("Usuario o passwords incorrectos");
         }
+
+        apiKeyRepository.invalidateUserKeys(user);
 
         String api_key_no_hash = ApiKeyGenerator.generateApiKey();
         ApiKeyEntity apiKey = new ApiKeyEntity();
         apiKey.setApiKey(hashser.sha256Hash(api_key_no_hash));
         apiKey.setUser(user);
+        apiKey.setValidated(true);
+        apiKey.setCreatedAT(Instant.now());
         apiKeyRepository.save(apiKey);
-        return  api_key_no_hash;
+        return api_key_no_hash;
 
     }
 
-    public UserEntity validateApiKey(String apiKey){
+    public UserEntity validateApiKey(String apiKey) {
 
         String hashed_apiKey = hashser.sha256Hash(apiKey);
 
-        if(!apiKeyRepository.existsByApiKey(hashed_apiKey)){
+        if (!apiKeyRepository.existsByApiKey(hashed_apiKey)) {
             return null;
         }
-        UserEntity user = apiKeyRepository.findByApiKey(hashed_apiKey);
+        ApiKeyEntity userapiKey = apiKeyRepository.findByApiKey(hashed_apiKey);
+        if (!userapiKey.isValidated()) {
+            return null;
+        }
+
+        UserEntity user = apiKeyRepository.findUserByApiKey(hashed_apiKey);
         System.out.println(user.getUsername());
         return user;
 
