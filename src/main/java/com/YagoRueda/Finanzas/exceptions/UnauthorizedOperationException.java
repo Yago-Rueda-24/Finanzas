f3402c586dc1bbcbbeea38b982897796cad8564a
yago rueda <yago.rc.04@gmail.com>
2025-09-24 18:28:24 +0200

    Creación de una excepción que representa un error generado por un usuario intentado realizar operaciones para las que no tiene permiso

---
+++ src/main/java/com/YagoRueda/Finanzas/exceptions/UnauthorizedOperationException.java
@@ -0,0 +1,7 @@
+package com.YagoRueda.Finanzas.exceptions;
+
+public class UnauthorizedOperationException extends Exception {
+    public UnauthorizedOperationException(String message) {
+        super(message);
+    }
+}
