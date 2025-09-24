9763ac5ceda2e5426821d5b64aeb091dd375c104
yago-vasco <yago.rc.04@gmail.com>
2025-09-24 12:09:04 +0200

    Implementación del controlador de creación de transacciones

--- src/main/java/com/YagoRueda/Finanzas/controllers/TransactionController.java
+++ src/main/java/com/YagoRueda/Finanzas/controllers/TransactionController.java
@@ -1,12 +1,17 @@
 package com.YagoRueda.Finanzas.controllers;
 
 import com.YagoRueda.Finanzas.DTOs.TransactionDTO;
+import com.YagoRueda.Finanzas.entities.TransactionEntity;
 import com.YagoRueda.Finanzas.entities.UserEntity;
+import com.YagoRueda.Finanzas.exceptions.InputTransactionException;
 import com.YagoRueda.Finanzas.services.TransactionService;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.http.HttpResponse;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/transaction")
@@ -21,12 +26,15 @@ public class TransactionController {
 
 
     @PostMapping()
-    public HttpResponse<?> saluda(HttpServletRequest request, @RequestBody TransactionDTO dto){
+    public ResponseEntity<?> saluda(HttpServletRequest request, @RequestBody TransactionDTO dto){
 
         UserEntity user = (UserEntity) request.getAttribute("authenticatedUser");
 
-        transactionService.create(user,dto);
-
-        return null;
+        try {
+            TransactionEntity transaction = transactionService.create(user,dto);
+            return ResponseEntity.status(HttpStatus.OK).body(transaction);
+        } catch (InputTransactionException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getErrors());
+        }
     }
 }
