f28835e87f240ee404ecfba35418e5f26f83374d
yago rueda <yago.rc.04@gmail.com>
2025-09-24 18:29:21 +0200

    Implementación del controlador de borrado de transacciones

--- src/main/java/com/YagoRueda/Finanzas/controllers/TransactionController.java
+++ src/main/java/com/YagoRueda/Finanzas/controllers/TransactionController.java
@@ -4,11 +4,14 @@ import com.YagoRueda.Finanzas.DTOs.TransactionDTO;
 import com.YagoRueda.Finanzas.entities.TransactionEntity;
 import com.YagoRueda.Finanzas.entities.UserEntity;
 import com.YagoRueda.Finanzas.exceptions.InputTransactionException;
+import com.YagoRueda.Finanzas.exceptions.UnauthorizedOperationException;
 import com.YagoRueda.Finanzas.services.TransactionService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/transaction")
@@ -45,8 +48,23 @@ public class TransactionController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getErrors());
         }catch (IllegalArgumentException e){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (UnauthorizedOperationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
         }
 
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<?> delete(HttpServletRequest request, @RequestParam long id){
+        UserEntity user = (UserEntity) request.getAttribute("authenticatedUser");
+        try {
+            transactionService.delete(user,id);
+            return ResponseEntity.status(HttpStatus.OK).body(Map.of("message","Elemento borrado con exito"));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (UnauthorizedOperationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
     }
 
 }
