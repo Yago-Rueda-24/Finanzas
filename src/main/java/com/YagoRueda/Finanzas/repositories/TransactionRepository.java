56623c5c163a9040acd7dcc5790b2b64d58053ab
yago rueda <yago.rc.04@gmail.com>
2025-10-01 15:50:19 +0200

    Implementación de la query

--- src/main/java/com/YagoRueda/Finanzas/repositories/TransactionRepository.java
+++ src/main/java/com/YagoRueda/Finanzas/repositories/TransactionRepository.java
@@ -1,7 +1,14 @@
 package com.YagoRueda.Finanzas.repositories;
 
 import com.YagoRueda.Finanzas.entities.TransactionEntity;
+import com.YagoRueda.Finanzas.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<TransactionEntity,Long> {
+
+    List<TransactionEntity> findByUserAndDateBetween(UserEntity user, LocalDate start, LocalDate end);
+
 }
