e2be2725addd541e14253c19094dd1afb42d5ebb
yago-vasco <yago.rc.04@gmail.com>
2025-10-14 10:23:39 +0200

    Implementación de la función de calculo de la media de gasto de cada dia de la semana en un mes

--- src/main/java/com/YagoRueda/Finanzas/services/AnaliticsService.java
+++ src/main/java/com/YagoRueda/Finanzas/services/AnaliticsService.java
@@ -8,9 +8,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.util.List;
-import java.util.Map;
+import java.time.YearMonth;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.DoubleAdder;
@@ -28,6 +29,7 @@ public class AnaliticsService {
     /**
      * Comprueba que la fecha de entrada introducida como String este en un formato valido, lo parsea a LocalDate y retorna un LocalDate con valor 01/MM/AAAA
      * siendo MM y AAAA el mes y año pasado en el string
+     *
      * @param date String que representa un mes con formato MM/AAAA
      * @return LocalDate que representa la el primer dia del mes/Año introducidos en formato 01/MM/AAAA
      * @throws IllegalArgumentException
@@ -76,6 +78,24 @@ public class AnaliticsService {
         }
 
         return startDate = LocalDate.of(year, month, 1);
+    }
+
+    private int daysOfWeekInMonth(LocalDate monthYear, DayOfWeek day) {
+        int month = monthYear.getMonth().getValue();
+        int year = monthYear.getYear();
+        YearMonth yearMonth = YearMonth.of(year, month);
+        int totalDias = yearMonth.lengthOfMonth();
+
+        int contador = 0;
+
+        for (int dia = 1; dia <= totalDias; dia++) {
+            LocalDate fecha = LocalDate.of(year, month, dia);
+            if (fecha.getDayOfWeek() == day) {
+                contador++;
+            }
+        }
+
+        return contador;
     }
 
     public BalanceDTO monthlyBalance(UserEntity user, String date) throws IllegalArgumentException {
@@ -138,9 +158,57 @@ public class AnaliticsService {
         return dto;
     }
 
-    /*
-    public BalanceDTO monthlyAverageIncomeExpense(UserEntity user, String date) {
+    public BalanceDTO monthlyExpensePerDayofWeek(UserEntity user, String date) throws IllegalArgumentException {
 
+        LocalDate startDate;
+        try {
+            startDate = validateMonthInputDate(date);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+
+        LocalDate endDate = startDate.withDayOfMonth(startDate.lengthOfMonth());
+        List<TransactionEntity> transactions = transactionRepository.findByUserAndDateBetween(user, startDate, endDate);
+
+        List<Float> weekExpense = Collections.synchronizedList(new ArrayList<>(Arrays.asList(0f, 0f, 0f, 0f, 0f, 0f, 0f)));
+
+        int cores = Runtime.getRuntime().availableProcessors();
+        int numHilos = Math.min(transactions.size(), cores);
+        if (numHilos == 0) numHilos = 1;
+
+        //Calculo del tamaño de las sublistas
+        int sublistLength = transactions.size() / numHilos;
+
+        try (ExecutorService executor = Executors.newFixedThreadPool(numHilos)) {
+            for (int i = 0; i < numHilos; i++) {
+
+                int start = i * sublistLength;
+                int end = (i == numHilos - 1) ? transactions.size() : start + sublistLength;
+                executor.submit(() -> {
+                    for (int j = start; j < end; j++) {
+                        TransactionEntity itTrans = transactions.get(j);
+                        if (itTrans.getAmount() < 0) {
+                            int day = itTrans.getDate().getDayOfWeek().getValue();
+                            synchronized (weekExpense) {
+                                weekExpense.set(day - 1, weekExpense.get(day - 1) - itTrans.getAmount());
+                            }
+                        }
+                    }
+                });
+            }
+        }
+
+
+        BalanceDTO dto = new BalanceDTO();
+        for(int i =0;i<weekExpense.size();i++){
+            DayOfWeek day = DayOfWeek.values()[i];
+            int numdays = daysOfWeekInMonth(startDate,day);
+            weekExpense.set(i,weekExpense.get(i)/numdays);
+        }
+
+        dto.setAverageExpensePerDayofWeek(weekExpense);
+
+        return dto;
     }
-*/
+
 }
