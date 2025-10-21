39d2ce461432130fe7b898de8a58a778c090050c
yago rueda <yago.rc.04@gmail.com>
2025-10-21 09:53:08 +0200

    Cambios en la función de checkbox

--- src/main/resources/static/js/funciones.js
+++ src/main/resources/static/js/funciones.js
@@ -1,6 +1,9 @@
-const checkbox = document.getElementById("CheckBoxLegal");
-const boton = document.getElementById("ButtonLogin");
+document.addEventListener("DOMContentLoaded", () => {
+  const checkbox = document.getElementById("CheckBoxLegal");
+  const boton = document.getElementById("ButtonLogin");
 
-checkbox.addEventListener("change", () => {
+  checkbox.addEventListener("change", () => {
     boton.disabled = !checkbox.checked;
+    console.log("Checkbox state changed:", checkbox.checked);
+  });
 });
